68ca0e59a0ff0307c0998804af248109597314c9
zhenzhenChange <1506678296@qq.com>
2020-05-31 22:20:54 +0800

    LeetCode：0101

---
+++ LeetCode/OneQuestionADay【5月】/0531.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 0101.对称二叉树
+ * 
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ * 
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  function check(L, R) {
+    if (!L && !R) return true;
+    if (!L || !R) return false;
+
+    return L.val == R.val && check(L.left, R.right) && check(L.right, R.left);
+  }
+
+  return check(root, root);
+};
