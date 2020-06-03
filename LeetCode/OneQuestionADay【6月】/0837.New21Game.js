0ba7ce02632f159ae6c3c6135f688d9b93a19733
zhenzhenChange <1506678296@qq.com>
2020-06-03 11:50:04 +0800

    LeetCode：0837 / Rename 1431 / Rename 64（Interview Question）

---
+++ LeetCode/OneQuestionADay【6月】/0837.New21Game.js
@@ -0,0 +1,26 @@
+/**
+ * 时间复杂度：O(min(N, K + W))
+ * 空间复杂度：O(K + W)
+ *
+ * @param {number} N
+ * @param {number} K
+ * @param {number} W
+ * @return {number}
+ */
+var new21Game = function (N, K, W) {
+  const dp = new Array(K + W + 1).fill(0.0);
+
+  for (let i = K; i <= N && i < K + W; i++) dp[i] = 1.0;
+
+  dp[K - 1] = (1.0 * Math.min(N - K + 1, W)) / W;
+
+  for (let i = K - 2; i >= 0; i--) dp[i] = dp[i + 1] - (dp[i + W + 1] - dp[i + 1]) / W;
+
+  return dp[0].toFixed(5);
+};
+
+var N = 28;
+var K = 17;
+var W = 10;
+
+console.log(new21Game(N, K, W));
