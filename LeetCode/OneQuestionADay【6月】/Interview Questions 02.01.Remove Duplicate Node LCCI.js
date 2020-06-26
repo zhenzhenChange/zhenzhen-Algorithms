ac8e92f5897556d745f23b5d2defed861cfc3391
zhenzhenChange <1506678296@qq.com>
2020-06-26 21:14:28 +0800

    LeetCode：Interview Questions 02.01

---
+++ LeetCode/OneQuestionADay【6月】/Interview Questions 02.01.Remove Duplicate Node LCCI.js
@@ -0,0 +1,51 @@
+/**
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ *
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeDuplicateNodes = function (head) {
+  if (!head) return head;
+
+  const set = new Set();
+
+  let prev = head;
+  let node = head.next;
+
+  set.add(head.val);
+
+  while (node) {
+    if (set.has(node.val)) {
+      prev.next = node.next;
+    } else {
+      set.add(node.val);
+      prev = prev.next;
+    }
+
+    node = node.next;
+  }
+
+  return head;
+};
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+const l1 = new ListNode(1);
+const l2 = new ListNode(2);
+const l3 = new ListNode(3);
+const l4 = new ListNode(3);
+const l5 = new ListNode(2);
+const l6 = new ListNode(1);
+
+l1.next = l2;
+l2.next = l3;
+l3.next = l4;
+l4.next = l5;
+l5.next = l6;
+
+console.log(l1);
+console.log(removeDuplicateNodes(l1));
