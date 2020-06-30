c59badc73c6820d8d0fbf093ed347ac0cc2ef673
zhenzhenChange <1506678296@qq.com>
2020-06-30 09:36:03 +0800

    LeetCode：Sword Finger Offer 09

---
+++ LeetCode/OneQuestionADay【6月】/Sword Finger Offer 09.Implement Queue With Two Stacks.js
@@ -0,0 +1,23 @@
+/**
+ * 时间复杂度：O(1)
+ * 空间复杂度：O(n)
+ */
+var CQueue = function () {
+  this.queue = [];
+};
+
+/**
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function (value) {
+  this.queue[this.queue.length] = value;
+};
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function () {
+  if (this.queue.length == 0) return -1;
+  return this.queue.shift();
+};
